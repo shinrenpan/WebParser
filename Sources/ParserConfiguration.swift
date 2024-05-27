e99c137b7e25e77c85f89117fda01e9a983b8bd4
Shinren Pan <shinren.pan@gmail.com>
2024-05-27 23:13:32 +0800

    ParserConfiguration properties from let to var

--- Sources/ParserConfiguration.swift
+++ Sources/ParserConfiguration.swift
@@ -9,35 +9,34 @@ import Foundation
 /// 爬取網頁的設置.
 public struct ParserConfiguration {
     /// 要爬取的 URL Request.
-    public let request: URLRequest
+    public var request: URLRequest
     
     /// 網頁瀏覽尺寸.
     ///
     /// 有些網頁會對應不同的尺寸, 導向不同的網址, 例如小尺寸就導向 mobile web.
     ///
     /// 為了正確的爬取, 請設置正確的瀏覽尺寸.
-    public let windowSize: CGSize
+    public var windowSize: CGSize
     
     /// 網頁瀏覽的 user agent.
     ///
     /// 有些網頁除了看尺寸大小, 還會看瀏覽器的 user agent, 導向不同網址,
     ///
     /// 為了正確的爬取, 請設置正確的 user agent.
-    public let customUserAgent: String?
+    public var customUserAgent: String?
     
     /// 重試次數.
     ///
     /// 最後 timeout 時間為 retryCount * retryDuration.
-    public let retryCount: UInt
+    public var retryCount: UInt
     
     /// 重試的間隔秒數.
     ///
     /// 最後 timeout 時間為 retryCount * retryDuration.
-    public let retryDuration: UInt
+    public var retryDuration: UInt
     
     /// 要執行的 javascript.
-    public let javascript: String
-    
+    public var javascript: String
     
     /// 初始化爬取設置
     /// - Parameters:
